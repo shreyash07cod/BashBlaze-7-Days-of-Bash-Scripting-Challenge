82f4b6420f0ff74970a555d5a1ff2754f647be9d
Shreyash sinha <72395959+shreyash07cod@users.noreply.github.com>
2023-08-09 14:51:36 +0530

    Add files via upload
    BashBlaze challange day 1

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This bash script contains basic commands in the form of tasks.
+
+# Task 2
+
+echo "I am learning bash scripting with TWS"
+
+#Task 3 Variables
+
+#echo $BASH
+
+name="Shreyash"
+echo "Hello i am ${name}"
+
+#Task 4 using variables
+
+name="Shreyash"
+echo "Hello ${name}, Please enter your age:"
+
+read age
+
+echo "My age is ${age}"
+
+#Task 5 Built-in variables
+
+echo "My current bash path - $BASH"
+echo "bash version i am using - $BASH_VERSION"
+echo "PID of bash i am running - $$"
+echo "Current directory - $PWD"
+echo "Hostname - $HOSTNAME"
+
+#Task 6 WildCard
+
+echo "civo" > company.txt
+
+
